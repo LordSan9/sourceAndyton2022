87e8483ca3036754daf04057b2726f43afc1b626
LordSan9 <104519846+LordSan9@users.noreply.github.com>
2022-07-24 03:17:46 +0300

    Update decorators.py

--- sbb_b/core/decorators.py
+++ sbb_b/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "فقط مالك البوت يمكنه استخدامي \n\n قم بتنصيب جمثون بنفسك."
+                or "فقط مالك البوت يمكنه استخدامي \n\n قم بتنصيب سورس اندي من خلال مراسله المبرمج @IIlAndylII ."
             )
             await c_q.answer(
                 HELP_TEXT,
